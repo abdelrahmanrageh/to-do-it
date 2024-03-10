1ef0d8af71c4e0f7475376aebc7e19df73bd7ef9
abdorageh <abdelrahmanrageh@gmail.com>
2024-03-10 23:53:35 +0200

    Uplode

--- js/2.js
+++ js/2.js
@@ -1,18 +1,31 @@
+checkTasks();
+
 //dark theme switch
 let darkSwitch = document.querySelector('.dark-switch');
 window.localStorage.darkMode === 'true'
+
+if(window.localStorage.darkMode === 'true'){
+    document.body.setAttribute('data-bs-theme', 'dark');
+    darkSwitch.setAttribute('checked' , '');
+}
+else if(window.localStorage.darkMode === 'false' || !window.localStorage.darkMode){
+    document.body.setAttribute('data-bs-theme', 'light');
+    darkSwitch.removeAttribute('checked');
+}
 
 darkSwitch.addEventListener('click', function() {
     if(window.localStorage.darkMode === 'true'){
         window.localStorage.setItem('darkMode', 'false');
         darkSwitch.removeAttribute('checked');
         document.body.setAttribute('data-bs-theme', 'light');
-
+        document.querySelector('body::before').style.backgroundColor = '#ffffffd5';
     }
-    else{
+    else if(window.localStorage.darkMode === 'false' || !window.localStorage.darkMode){
         darkSwitch.setAttribute('checked' , '');
         window.localStorage.setItem('darkMode', 'true');
         document.body.setAttribute('data-bs-theme', 'dark');
+        // document.querySelector('body::before').style.backgroundColor = '#212529f1';
+        
     }
 });
 
@@ -30,8 +43,6 @@ if(window.localStorage.tasks){
     for(let i of tasksArray){
         addTaskTpage(i.taskValue , i.taskTitle , i.dateAdded);
     }
-} if(!window.localStorage.tasks){
-    document.querySelector('.tasks-label').style.display = 'none';
 }
 
 //add tasks to the array
@@ -51,7 +62,6 @@ function addTaskTpage(taskValue , taskTitle , dateAdded){
     //create the task
     let parent = document.createElement('a');
     parent.classList.add('list-group-item' , 'list-group-item-action' );
-    
     
     let taskDiv = document.createElement('div');
     taskDiv.classList.add('d-flex', 'w-100', 'justify-content-between');
@@ -80,9 +90,14 @@ function addTaskTpage(taskValue , taskTitle , dateAdded){
     //details of the task(task value, checkbox and trash icon)
     let detailsDiv = document.createElement('div');
     
+    let checkBoxLabel = document.createElement('label');
+    checkBoxLabel.classList.add('form-check-label');
+
     let checkBox = document.createElement('input');
     checkBox.type = 'checkbox';
     checkBox.classList.add('form-check-input' , 'done');
+
+    checkBoxLabel.appendChild(checkBox);
 
     flexDiv.appendChild(taskDate);
     flexDiv.appendChild(trashIcon);
@@ -92,12 +107,15 @@ function addTaskTpage(taskValue , taskTitle , dateAdded){
     detailsDiv.appendChild(flexDiv);
 
     taskDiv.appendChild(taskName);
+    taskDiv.appendChild(checkBoxLabel)
 
     parent.appendChild(taskDiv);
     parent.appendChild(detailsDiv);
     
     //append the task to the list of tasks
     listGroup.appendChild(parent);
+
+    checkTasks();
 }
 
 //passing data from form to the array(when clicking the add button)
@@ -119,6 +137,7 @@ addBtn.addEventListener('click', function() {
         taskInput.classList.remove('is-invalid');
         taskTitle.classList.remove('is-invalid');       
     }
+    checkTasks();
 });
 
 //handle the enter key press
@@ -131,57 +150,60 @@ function handleKeyPress(element){
         }
     });
 };
+
 handleKeyPress(taskTitle);
 handleKeyPress(taskInput);
 
 //events of clicking on tasks
 listGroup.addEventListener('click', function(event){
-    if(event.value === 'click'){
-        let task = event.target.closest('a');
-        
-        // deleteTask();
-        if(event.target.classList.contains('fa-trash')){
-            let deleteTarget = event.target.parentElement.children[0].innerText;
-            for(let everyTask of tasksArray){
-                if(everyTask.dateAdded === deleteTarget.slice(10)){
-                    tasksArray.splice(tasksArray.indexOf(everyTask), 1);
-                    window.localStorage.tasks = JSON.stringify(tasksArray);
-                }
+    
+    
+    let task = event.target.closest('a');
+    // deleteTask();
+    if(event.target.classList.contains('fa-trash')){
+        let deleteTarget = event.target.parentElement.children[0].innerText;
+        for(let everyTask of tasksArray){
+            if(everyTask.dateAdded === deleteTarget.slice(10)){
+                tasksArray.splice(tasksArray.indexOf(everyTask), 1);
+                window.localStorage.tasks = JSON.stringify(tasksArray);
             }
-            task.style.opacity = '0';
-            task.style.height = '0';
-            task.style.padding = '0';
-            task.style.margin = '0';
-            setTimeout(() => {
-                task.remove();}
-            , 500);
         }
-
-        //mark as done
-        else if(event.target.type === 'checkbox'){
-            return;
-        }
-
-        //collapse the task 
-        else if(task.classList.contains('active')){
-            task.classList.remove('active');
-            task.children[1].style.visibility = 'hidden';
-            task.children[1].style.opacity = '0';
-            task.style.height = '50px';
-
-        }
-        //expand the task
-        else{
-            task.classList.add('active');
-            task.children[1].children[0].style.display = 'block';
-            task.children[1].style.visibility = 'visible';
-            task.children[1].style.opacity = '1';
-            task.style.height = +task.scrollHeight + 25 + "px";
-        }
+        task.style.opacity = '0';
+        task.style.height = '0';
+        task.style.padding = '0';
+        task.style.margin = '0';
+        setTimeout(() => {
+            task.remove();}
+        , 500);
     }
+    else if(event.target.type === 'checkbox'){
+        return;
+    }
+    //collapse the task 
+    else if(task.classList.contains('active')){
+        task.classList.remove('active');
+        task.children[1].style.visibility = 'hidden';
+        task.children[1].style.opacity = '0';
+        task.style.height = '50px';
+
+    }
+    //expand the task
+    else{
+        task.classList.add('active');
+        task.children[1].children[0].style.display = 'block';
+        task.children[1].style.visibility = 'visible';
+        task.children[1].style.opacity = '1';
+        task.style.height = +task.scrollHeight + 25 + "px";
+    }
+    checkTasks();
 });
 
-
-
+function checkTasks(){
+    if(window.localStorage.tasks === '[]'){
+        document.querySelector('.tasks-label').style.opacity = '0';
+    }else{
+        document.querySelector('.tasks-label').style.opacity = '1';
+    }
+}
 
 
